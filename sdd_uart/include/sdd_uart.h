f03d92d975779c2e55e90dacc765be7417165638
erlingrj <erling.jellum@gmail.com>
2023-05-08 00:33:25 +0200

    Format docs

--- sdd_uart/include/sdd_uart.h
+++ sdd_uart/include/sdd_uart.h
@@ -17,14 +17,14 @@ typedef struct {
   const int ns_per_bit;
   const cbuf_t *cbuf;
   const lock_t lock;
-} sdd_uart_private_config_t
+} sdd_uart_private_config_t;
 
-    /**
-     * @brief This is the user-facing configuration structure for sdd_uart.
-     * The fields, except `__misc`, should be set by the user before
-     *
-     */
-    typedef struct {
+/**
+ * @brief This is the user-facing configuration structure for sdd_uart.
+ * The fields, except `__misc`, should be set by the user before
+ *
+ */
+typedef struct {
   bool initialized;
   uint32_t port;
   size_t pin;
@@ -43,8 +43,9 @@ typedef struct {
 fp_ret_t sdd_uart_tx_service_run(sdd_uart_config_t *uart);
 
 /**
- * @brief This function is non-blocking and schedules the transmission of a byte.
- * This assumes that that the tx_service has been started and is up-and-running
+ * @brief This function is non-blocking and schedules the transmission of a
+ * byte. This assumes that that the tx_service has been started and is
+ * up-and-running
  * @param uart
  * @param byte
  * @param len
@@ -53,29 +54,30 @@ fp_ret_t sdd_uart_tx_service_run(sdd_uart_config_t *uart);
 fp_ret_t sdd_uart_tx_service_send(sdd_uart_config_t *uart, char *byte,
                                   size_t len);
 /**
- * @brief Start a UART receiver service on the current HART. This function should not return
- * 
- * @param uart 
- * @return fp_ret_t 
+ * @brief Start a UART receiver service on the current HART. This function
+ * should not return
+ *
+ * @param uart
+ * @return fp_ret_t
  */
 fp_ret_t sdd_uart_rx_service_run(sdd_uart_config_t *uart);
 
 /**
  * @brief Fetch received data from the rx_service.
- * 
- * @param uart 
- * @param byte 
- * @return fp_ret_t 
+ *
+ * @param uart
+ * @param byte
+ * @return fp_ret_t
  */
 fp_ret_t sdd_uart_rx_service_recv(sdd_uart_config_t *uart, char *byte);
 
 /**
  * @brief This is a non-service function. It does a blocking receive
  * of data from the external pin. This might block indefinetly.
- * 
- * @param uart 
- * @param byte 
- * @return fp_ret_t 
+ *
+ * @param uart
+ * @param byte
+ * @return fp_ret_t
  */
 fp_ret_t sdd_uart_blocking_recv(sdd_uart_config_t *uart, char *byte);
 
@@ -83,13 +85,11 @@ fp_ret_t sdd_uart_blocking_recv(sdd_uart_config_t *uart, char *byte);
  * @brief This is a non-service function. It performs a send operation.
  * It is blocking in the sense that it is the caller which is actually
  * performing the bit-banging which results in sending
- * 
- * @param uart 
- * @param byte 
- * @return fp_ret_t 
+ *
+ * @param uart
+ * @param byte
+ * @return fp_ret_t
  */
 fp_ret_t sdd_uart_blocking_send(sdd_uart_config_t *uart, char byte);
-
-
 
 #endif
